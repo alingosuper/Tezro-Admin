9a17d31ff6e415214641348cffdfd3a718b588f8
alingosuper <alingosuper@gmail.com>
2026-03-25 07:04:05 +0500

    Fix: Removed duplicate main.jsx and updated index.html path

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import AdminDashboard from "./screens/Admin/AdminDashboard"
-import AdminLogin from "./screens/Auth/AdminLogin"
-import "./global.css"
-
-const router = createBrowserRouter([
-  { path: "/", element: <AdminDashboard /> },
-  { path: "/login", element: <AdminLogin /> }
-]);
+import App from './App'
+import './global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
