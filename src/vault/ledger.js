fa3094585b07f703f2a8b807d503d5e2c2f2ee3a
alingosuper <alingosuper@gmail.com>
2026-04-02 11:25:27 +0500

    saving local work before pull

--- src/vault/ledger.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto');
-
-const processTransaction = (data) => {
-    console.log("Transaction Securely Processed in Core...");
-    return { status: "Encrypted", timestamp: Date.now() };
-};
-
-module.exports = { processTransaction };
